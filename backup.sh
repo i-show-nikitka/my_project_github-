104f1ab51680c693fd8d27908e22cd7a738fbe8c
Nikita <nikita@gmail.com>
2026-03-03 23:00:53 +0300

    Add backup script

---
+++ backup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Backup script for my project
+# Author: Nikita
+# Date: 2026
+
+echo "Starting backup process..."
+echo "Creating backup archive..."
+echo "Backup completed successfully!"
